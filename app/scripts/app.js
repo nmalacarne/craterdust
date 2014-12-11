315ee6933b1b19314f657b3728d668c4468d7abc
Nicholas Malacarne <nick@craterdust.com>
2014-12-11 17:01:21 -0500

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name craterdustApp
+ * @description
+ * # craterdustApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('craterdustApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'duScroll'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name craterdustApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the craterdustApp
+ */
+angular.module('craterdustApp')
+  .controller('MainCtrl', function () {
+      // TODO: logic
+  });
