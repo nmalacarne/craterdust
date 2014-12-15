7b8ddfc8245f0c94f22e959155f6246ed8b77d6a
Nicholas Malacarne <nick@craterdust.com>
2014-12-15 12:05:16 -0500

    remove ngModal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the craterdustApp
  */
 angular.module('craterdustApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $rootScope) {
     $http.get('../../data/portfolio.json').success(function(data) {
       $scope.portfolio = data;
     });
