bee0ae682e452944ab4ada30aa903b2a13404799
Nicholas Malacarne <nick@craterdust.com>
2015-02-02 10:11:53 -0500

    removed 'selected' methods and member variables; added load method

--- app/scripts/services/portfolio.js
+++ app/scripts/services/portfolio.js
@@ -10,12 +10,6 @@
 angular.module('craterdustApp')
   .service('Portfolio', function ($http) {
     var items = [];
-    var selected = null;
-
-    // load portfolio data
-    $http.get('../../data/portfolio.json').success(function(data) {
-      items = data;
-    });
 
     return {
       getItems: function() {
@@ -27,11 +21,10 @@ angular.module('craterdustApp')
       getSelected: function() {
         return selected
       },
-      select: function(index) {
-        selected = items[index];
-      },
-      deselect: function() {
-        selected = null;
+      load: function() {
+        $http.get('../../data/portfolio.json').success(function(data) {
+          items = data;
+        });
       }
     };
   });
